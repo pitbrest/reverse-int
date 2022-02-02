363e8975132266f3f54a3f0785286c80680844c3
pitbrest <sasha.petrov@tut.by>
2022-02-02 09:59:09 +0300

    some commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	if(n < 0) {return Number(Math.abs(n).toString().split('').reverse().join(''))}
+	else {return Number(n.toString().split('').reverse().join(''))}
 }
+
